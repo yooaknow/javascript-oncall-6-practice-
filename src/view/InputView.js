72624bb685ce30c2ca9c5797850bf43a5ed3a558
yooaknow <dbdkfma1228@naver.com>
2026-01-07 23:16:34 +0900

    refactor(view): InputView / OutputView를 신규 구조에 맞게 수정
    - Controller 흐름에 맞게 입력 구조 정리
    - 출력 로직을 Console.print 위임으로 단순화

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,36 +1,23 @@
-/* 
-비상 근무를 배정할 월과 시작 요일을 입력하세요> 1,금
-평일 비상 근무 순번대로 사원 닉네임을 입력하세요> 준팍,도밥,고니,수아,루루,글로
-휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> 수아,수아,글로,고니,도밥,준팍
-[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.
-평일 비상 근무 순번대로 사원 닉네임을 입력하세요>
-*/ 
-
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
 
 const InputView = {
-  async readDate() {
-    const input = await Console.readLineAsync(
-      "비상 근무를 배정할 월과 시작 요일을 입력하세요>"
+  readMonthAndStartDay() {
+    return Console.readLineAsync(
+      '비상 근무를 배정할 월과 시작 요일을 입력하세요> ',
     );
-    return input;
   },
 
-  async readweekday() {
-    const input = await Console.readLineAsync(
-      "평일 비상 근무 순번대로 사원 닉네임을 입력하세요>"
+  readWeekdayOrder() {
+    return Console.readLineAsync(
+      '평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
     );
-    return input;
-  },
-  
-    async readweekend() {
-    const input = await Console.readLineAsync(
-      "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>"
-    );
-    return input;
   },
 
-
+  readHolidayOrder() {
+    return Console.readLineAsync(
+      '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
+    );
+  },
 };
 
 export default InputView;
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,13 +1,9 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
-  printPreview(Month, day, week, name) {
-    Console.print(`${Month} ${day}일 ${week} ${name} `);
-  },
-
-  printError(message) {
+  print(message) {
     Console.print(message);
-  }
+  },
 };
 
 export default OutputView;
