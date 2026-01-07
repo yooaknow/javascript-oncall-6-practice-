46c971366c052f3960c4e69ebaeee542deef8a74
yooaknow <dbdkfma1228@naver.com>
2026-01-07 23:16:38 +0900

    refactor(app): App에서 OnCallController 실행하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import OnCallController from './controller/OnCallController.js';
+
 class App {
-  async run() {}
+  async run() {
+    const controller = new OnCallController();
+    await controller.run();
+  }
 }
 
 export default App;
